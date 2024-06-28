ddcfe618bc1748288202c23dd54867709941dde9
omkar <omkarjadhav095@gmail.com>
2024-06-28 16:13:37 +0530

    tailwind setup is done along with navbar component.

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,19 @@
+// src/components/Navbar.tsx
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-blue-600 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-xl">My App</div>
+        <div className="space-x-4">
+          <a href="#" className="text-white">Home</a>
+          <a href="#" className="text-white">About</a>
+          <a href="#" className="text-white">Contact</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
